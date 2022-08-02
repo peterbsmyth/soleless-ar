559ce5bbaeae2f7cc40f7c4f466c3581bf15c82c
Peter Smith <peter@upstateinteractive.io>
2022-08-02 12:09:53 -0400

    dynamic images

---
+++ main.js
@@ -0,0 +1,27 @@
+const params = new Proxy(new URLSearchParams(window.location.search), {
+  get: (searchParams, prop) => searchParams.get(prop),
+});
+const code = params.code;
+
+const request = fetch(`https://api.peterbssmyth.com/soleless/card/${code}/ar-url`, {
+  method: 'GET',
+  headers: {
+    'content-type': 'application/json'
+  }
+});
+
+request
+  .then(response => {
+    if (response.status !== 200) {
+      alert("Sorry, something went wrong. Please try again later. Did you use a valid code?");
+      return; 
+    }
+    return response.json();
+    
+  })
+  .then(({ url }) => {
+    document.querySelector('#card').setAttribute('src', url);
+  })
+  .catch(() => {
+    document.querySelector('#card').setAttribute('src', '/warning.png');
+  });
