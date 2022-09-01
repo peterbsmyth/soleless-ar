7c26a3e34e9cfb2fe26cc1dc1a0111127dfa6db5
pete smith <pete@petes-MacBook-Pro.local>
2022-08-31 23:12:37 -0700

    sentient carmelo

--- main.js
+++ main.js
@@ -1,27 +1,50 @@
-const params = new Proxy(new URLSearchParams(window.location.search), {
-  get: (searchParams, prop) => searchParams.get(prop),
-});
-const code = params.code;
-
-const request = fetch(`https://api.peterbsmyth.com/soleless/card/${code}/ar-url`, {
-  method: 'GET',
-  headers: {
-    'content-type': 'application/json'
-  }
-});
-
-request
-  .then(response => {
-    if (response.status !== 200) {
-      alert("Sorry, something went wrong. Please try again later. Did you use a valid code?");
-      return; 
-    }
-    return response.json();
-    
-  })
-  .then(({ url }) => {
-    document.querySelector('#card').setAttribute('src', url);
-  })
-  .catch(() => {
-    document.querySelector('#card').setAttribute('src', '/warning.png');
+const run = async () => {
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
   });
+  const code = params.code;
+
+  if (!code) {
+    document.querySelector('#card').play();
+    return;
+  }
+
+  const codeParam = {
+    filter: {
+      property: 'code',
+      rich_text: {
+        contains: code,
+      },
+    },
+  };
+
+  const cardsRequest = await fetch(
+    `https://api.myth.software/soleless/cards?` +
+      new URLSearchParams({
+        where: JSON.stringify(codeParam),
+      }),
+    {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+      },
+    }
+  );
+  const cards = await cardsRequest.json();
+  const key = Object.keys(cards[0]).find((key) => key.includes('animation'));
+  const animationId = cards[0][key][0].id;
+
+  const animationRequest = await fetch(
+    `https://api.myth.software/soleless/animations/${animationId}`,
+    {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+      },
+    }
+  );
+  const { animation } = await animationRequest.json();
+  document.querySelector('#card').setAttribute('src', animation);
+  document.querySelector('#card').play();
+};
+run();
